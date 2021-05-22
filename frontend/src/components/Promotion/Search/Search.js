e46248bb8cb5fc59c6125ca92e42ff83779ad60b
TeodoroMorato <teodorotmmorato@gmail.com>
2021-05-21 23:32:12 -0300

    Search with UseEffects

---
+++ frontend/src/components/Promotion/Search/Search.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import PromotionCard from 'components/Promotion/Card/card';
+import { Link } from "react-router-dom";
+import './Search.css';
+import PromotionList from "../List/List";
+
+const PromotionSearch = () => {
+    const [promotions, setPromotions] = useState([]);
+    const [search, setSearch] = useState('');
+    useEffect(() => {
+        const params = {};
+        if (search) {
+            params.title_like = search
+        }
+        axios.get('http://localhost:5000/promotions?_embed=comments', { params }).then((response) => {
+            setPromotions(response.data);
+        });
+    }, [search]);
+    return (
+        <div className="promotion-search">
+            <header className="promotion-search_header">
+                <h1>Promo Show</h1>
+                <Link to="/create">Nova Promoção</Link>
+            </header>
+            <input
+                placeholder="Buscar"
+                type="search"
+                className="promotion-search_input"
+                value={search}
+                onChange={(ev) => setSearch(ev.target.value)}
+            />
+            <PromotionList promotions={promotions} loading={!promotions.length} />
+            
+        </div>
+    );
+};
+
+export default PromotionSearch;
--- frontend/src/pages/Promotion/Search/Search.js
+++ frontend/src/pages/Promotion/Search/Search.js
@@ -1,22 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import PromotionCard from 'components/Promotion/Card/card';
-import axios from 'axios';
+import PromotionSearch from 'components/Promotion/Search/Search';
+import UIContainer from 'components/UI/Container/Container';
 
 const PagesPromotionSearch = () => {
-  const [promotions, setPromotions] = useState([]);
-  useEffect(() => {
-    axios.get('http://localhost:5000/promotions?_embed=comments').then((response) => {
-      setPromotions(response.data);
-    });
-  }, []);
-
 
   return (
-    <div style={{ maxWidth: '800px', margin: '30px auto', }}>
-      {promotions.map((promotion) => (
-        <PromotionCard promotion={promotion} />
-      ))}
-    </div>
+    <UIContainer>
+      <PromotionSearch />
+    </UIContainer>
   );
 };
 
